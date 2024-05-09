7f0b49f410fa08213a3e60db2bf0a24a724c990d
hasanalam24 <hasanalam365@gmail.com>
2024-05-09 21:12:52 +0600

    Only Route setup and navber create

---
+++ src/Layouts/MainLayout.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Components/Navbar/Navbar";
+
+
+const MainLayout = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default MainLayout;

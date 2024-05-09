f118298c42bf6d250ff54f5f8a1d533af667170d
hasanalam24 <hasanalam365@gmail.com>
2024-05-10 00:58:42 +0600

    footer done

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,54 @@
+import { IoMdMailUnread } from "react-icons/io";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+const Footer = () => {
+    return (
+        <div>
+            <footer className="footer p-10 bg-base-200 text-base-content">
+                <aside>
+                    {/* <img className="w-[120px] h-[120px]" src="https://i.ibb.co/KNyrbGC/Human-Logo.png" alt="" /> */}
+                    <h1 className="text-2xl font-semibold">Human Plannet</h1>
+                    <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
+                </aside>
+                <nav>
+                    <h6 className="footer-title">Services</h6>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                    <a className="link link-hover">Advertisement</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Company</h6>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                    <a className="link link-hover">Press kit</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Legal</h6>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Contact</h6>
+                    <div className="flex gap-3 items-center justify-center">
+                        <IoMdMailUnread className="text-lg"></IoMdMailUnread>
+                        <p>hasanalam365@gmail.com</p>
+                    </div>
+                    <Link to="https://www.facebook.com/mdalam.islam.351/" className="flex gap-3 items-center justify-center">
+                        <FaFacebookSquare className="text-lg"></FaFacebookSquare>
+                        <FaXTwitter className="text-lg"></FaXTwitter>
+
+
+                    </Link>
+
+
+                </nav>
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,13 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
 
 
 
 const MainLayout = () => {
     return (
         <div className="container mx-auto">
+
             <Navbar></Navbar>
+
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };

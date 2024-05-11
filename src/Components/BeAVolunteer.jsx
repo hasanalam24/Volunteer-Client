d1296c60891596413388835fc6b419a818c7dc87
hasanalam24 <hasanalam365@gmail.com>
2024-05-11 23:43:34 +0600

    be a volunteer page css stype

--- src/Components/BeAVolunteer.jsx
+++ src/Components/BeAVolunteer.jsx
@@ -2,7 +2,8 @@ import { useContext } from "react";
 import { AppContext } from "../Firebase/AuthProvider";
 import { useLoaderData } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
-
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 
 const BeAVolunteer = () => {
@@ -53,84 +54,88 @@ const BeAVolunteer = () => {
 
     return (
         <div>
+            <div>
+                <img className="relative h-[1100px] md:h-[710px] lg:h-[730px] w-full " src="https://i.ibb.co/FhFYpmR/colin-cassidy-d0e-AMp-UPXgo-unsplash.jpg" alt="" />
+            </div>
             <Helmet>
-                <title>Human Plannet || Be A Volunteer</title>
+                <title>Human Plannet || Request Volunteer</title>
             </Helmet>
-
-            <section className="p-6 bg-gray-200 dark:bg-gray-200 dark:text-gray-900">
+            <section className="p-6 mt-12 mb-8 absolute top-10 w-[95%] mx-auto">
                 <form onSubmit={handleRequest} noValidate="" action="" className="container flex flex-col mx-auto space-y-1">
                     <div className="pl-6">
-                        <p className="font-medium">Request For Volunteer</p>
+                        <p className="font-medium text-white text-center text-4xl">Request Be A Volunteer Post</p>
 
                     </div>
-                    <fieldset className="grid grid-cols-4 gap-6 p-6 pt-2 rounded-md shadow-sm dark:bg-gray-50">
+                    <fieldset className=" gap-6 p-6 pt-2 rounded-md shadow-sm dark:bg-gray-50">
 
                         <div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
-                            <div className="col-span-full sm:col-span-3">
-                                <label className="text-md font-medium">Post Title</label>
-                                <input id="postTitle" type="text" name="postTile" value={postTile} placeholder="Post Title" className="w-full rounded-md p-2" />
+                            <div className="col-span-full sm:col-span-3 animate__animated animate__bounceInLeft">
+                                <label className="text-md font-medium text-white ">Post Title</label>
+                                <input id="postTitle" type="text" name="postTile"
+                                    value={postTile} placeholder="Post Title" className="w-full rounded-md p-2" />
                             </div>
-                            <div className="col-span-full sm:col-span-3">
-                                <label className="text-md font-medium">Thumbnail</label>
-                                <input id="thumbnail" type="text" name="thumbnail" placeholder="Thumbnail" value={thumbnail} className="w-full rounded-md p-2" />
+                            <div className="col-span-full sm:col-span-3 animate__animated animate__bounceInRight">
+                                <label className="text-md font-medium text-white">Thumbnail</label>
+                                <input id="thumbnail" type="text" name="thumbnail"
+                                    value={thumbnail} placeholder="Thumbnail" className="w-full rounded-md p-2" />
                             </div>
-                            <div className="col-span-full">
-                                <label className="text-md font-medium">Description</label>
-                                <textarea id="description" name="description" placeholder="Description" value={description} className="w-full rounded-md p-2"></textarea>
+                            <div className="col-span-full animate__animated animate__bounceInLeft">
+                                <label className="text-md font-medium text-white">Description</label>
+                                <textarea id="description" name="description"
+                                    value={description} placeholder="Description" className="w-full rounded-md p-2"></textarea>
                             </div>
-                            <div className="col-span-full sm:col-span-3">
-                                <label className="text-md font-medium">How Much Needed?</label>
-                                <input id="" type="text" value={needPeoples} name="needPeoples" placeholder="Example: 10" className="w-full rounded-md p-2" />
+                            <div className="col-span-full sm:col-span-3 animate__animated animate__bounceInLeft">
+                                <label className="text-md font-medium text-white">How Much Needed?</label>
+                                <input id="needVol" type="text"
+                                    value={needPeoples} name="needPeoples" placeholder="Example: 10" className="w-full rounded-md p-2" />
                             </div>
 
-                            <div className="col-span-full sm:col-span-3">
-                                <label className="text-md font-medium">Location</label>
-                                <input id="location" type="text" value={location} name="location" placeholder="Location" className="w-full rounded-md p-2" />
+                            <div className="col-span-full sm:col-span-3 animate__animated animate__bounceInRight">
+                                <label className="text-md font-medium text-white">Location</label>
+                                <input id="location" type="text"
+                                    value={location} name="location" placeholder="Location" className="w-full rounded-md p-2" />
                             </div>
-                            <div className="col-span-full sm:col-span-3">
-                                <label className="text-md font-medium">Selected Category</label>
-                                <input id="" type="text" value={category} name="category" placeholder="category" className="w-full rounded-md p-2" />
+                            <div className="col-span-full sm:col-span-3 animate__animated animate__bounceInLeft">
+                                <h1 className="text-md font-medium text-white">Select Category</h1>
+                                <select className="p-2 rounded-md" name="category" id="">
+                                    <option disabled selected>{category}</option>
+                                    <option value="Health Care">Health Care</option>
+                                    <option value="Education">Education</option>
+                                    <option value="Social Service">Social Service</option>
+                                    <option value="Animal Welfare">Animal Welfare</option>
+                                </select>
                             </div>
-                            <div className="col-span-full sm:col-span-3 flex flex-col">
-                                <label className="text-md font-medium">Deadline</label>
-                                <input id="" type="text" value={deadline} name="deadline" placeholder="deadline" className="w-full rounded-md p-2" />
+                            <div className="col-span-full sm:col-span-3 flex flex-col animate__animated animate__bounceInRight">
+                                <label htmlFor="" className="text-md font-medium text-white">Deadline</label>
+                                <DatePicker className="p-2 rounded-md" selected={deadline} onChange={(date) => setStartDate(date)}
+                                    format='yyyy-MM-dd' />
                             </div>
                         </div>
                     </fieldset>
-                    <div className="pl-6">
-                        <p className="font-medium text-lg">Your Information</p>
+                    <div className="w-[96%] mx-auto p-3 border-2 hover:bg-[#546e7a] ">
+                        <p className="font-medium text-2xl text-white text-center">Your Information</p>
 
                     </div>
                     <fieldset className="grid grid-cols-4 gap-6 p-6 pt-2 rounded-md shadow-sm dark:bg-gray-50">
 
-                        <div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
-                            <div className="col-span-full sm:col-span-3">
-                                <label htmlFor="username" className="text-md font-medium">Username</label>
+                        <div className="grid grid-cols-6 gap-4 col-span-full ">
+                            <div className="col-span-full sm:col-span-3 animate__animated animate__bounceInLeft">
+                                <label htmlFor="username" className="text-md font-medium text-white">Username</label>
                                 <input id="username" type="text" name="username"
                                     placeholder="Username" value={user?.displayName} className="w-full rounded-md p-2" />
                             </div>
-                            <div className="col-span-full sm:col-span-3">
-                                <label htmlFor="website" className="text-md font-medium">Email</label>
-                                <input id="email" type="email" name="requestEmail"
+                            <div className="col-span-6 sm:col-span-3 animate__animated animate__bounceInRight">
+                                <label htmlFor="" className="text-md font-medium text-white">Email</label>
+                                <input id="email" type="email" name="email"
                                     placeholder="Email" value={user?.email} className="w-full rounded-md p-2" />
-                            </div>
-                            <div className="col-span-full sm:col-span-3">
-                                <label htmlFor="website" className="text-md font-medium">Suggestion</label>
-                                <input id="" type="text" name="suggestion"
-                                    placeholder="Suggestion" className="w-full rounded-md p-2" />
-                            </div>
-                            <div className="col-span-full sm:col-span-3">
-                                <label htmlFor="website" className="text-md font-medium">Status</label>
-                                <input id="" type="text" name="status"
-                                    placeholder="Status" value="Requested" className="w-full rounded-md p-2" />
                             </div>
 
 
                         </div>
 
                     </fieldset>
-                    <div>
-                        <input className="btn bg-green-600 text-white w-full" type="submit" value="Request" />
+                    <div className="w-[96%] mx-auto">
+                        <input className="btn bg-sky-600 text-white w-full " type="submit" value="Request" />
                     </div>
                 </form>
             </section>

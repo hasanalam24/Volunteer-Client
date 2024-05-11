cd6660f31a669b7681da69925a5fd25e88994132
hasanalam24 <hasanalam365@gmail.com>
2024-05-11 23:25:17 +0600

    updated post animated  css

--- src/Pages/UpdatedPage.jsx
+++ src/Pages/UpdatedPage.jsx
@@ -5,6 +5,7 @@ import { useContext, useState } from "react";
 import { useLoaderData, useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 import { Helmet } from "react-helmet-async";
+import 'animate.css';
 
 const UpdatedPage = () => {
     const [startDate, setStartDate] = useState(new Date());
@@ -73,74 +74,78 @@ const UpdatedPage = () => {
 
     return (
         <div>
+            <div>
+                <img className="relative h-[1100px] md:h-[710px] lg:h-[730px] w-full " src="https://i.ibb.co/3B4dNmB/billy-huynh-W8-KTS-mh-FUE-unsplash.jpg" alt="" />
+            </div>
             <Helmet>
                 <title>Human Plannet || Updated</title>
             </Helmet>
-            <section className="p-6 bg-gray-200 dark:bg-gray-200 dark:text-gray-900 mt-12 mb-8">
+            <section className="p-6 mt-12 mb-8 absolute top-10 w-[95%] mx-auto">
                 <form onSubmit={handleUpdated} noValidate="" action="" className="container flex flex-col mx-auto space-y-1">
                     <div className="pl-6">
-                        <p className="font-medium">Update Volunteer Post Information</p>
+                        <p className="font-medium text-white text-center text-4xl">Update Volunteer Post</p>
 
                     </div>
-                    <fieldset className="grid grid-cols-4 gap-6 p-6 pt-2 rounded-md shadow-sm dark:bg-gray-50">
+                    <fieldset className=" gap-6 p-6 pt-2 rounded-md shadow-sm dark:bg-gray-50">
 
                         <div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
-                            <div className="col-span-full sm:col-span-3">
-                                <label className="text-md font-medium">Post Title</label>
-                                <input id="postTitle" type="text" name="postTile" placeholder="Post Title"
-                                    defaultValue={datas.postTile} className="w-full rounded-md p-2" />
+                            <div className="col-span-full sm:col-span-3 animate__animated animate__bounceInLeft">
+                                <label className="text-md font-medium text-white ">Post Title</label>
+                                <input id="postTitle" type="text" name="postTile"
+                                    defaultValue={datas.postTile} placeholder="Post Title" className="w-full rounded-md p-2" />
                             </div>
-                            <div className="col-span-full sm:col-span-3">
-                                <label className="text-md font-medium">Thumbnail</label>
+                            <div className="col-span-full sm:col-span-3 animate__animated animate__bounceInRight">
+                                <label className="text-md font-medium text-white">Thumbnail</label>
                                 <input id="thumbnail" type="text" name="thumbnail"
                                     defaultValue={datas.thumbnail} placeholder="Thumbnail" className="w-full rounded-md p-2" />
                             </div>
-                            <div className="col-span-full">
-                                <label className="text-md font-medium">Description</label>
+                            <div className="col-span-full animate__animated animate__bounceInLeft">
+                                <label className="text-md font-medium text-white">Description</label>
                                 <textarea id="description" name="description"
                                     defaultValue={datas.description} placeholder="Description" className="w-full rounded-md p-2"></textarea>
                             </div>
-                            <div className="col-span-full sm:col-span-3">
-                                <label className="text-md font-medium">How Much Needed?</label>
-                                <input id="needVol" type="text" name="needPeoples"
-                                    defaultValue={datas.needPeoples} placeholder="Example: 10" className="w-full rounded-md p-2" />
+                            <div className="col-span-full sm:col-span-3 animate__animated animate__bounceInLeft">
+                                <label className="text-md font-medium text-white">How Much Needed?</label>
+                                <input id="needVol" type="text"
+                                    defaultValue={datas.needPeoples} name="needPeoples" placeholder="Example: 10" className="w-full rounded-md p-2" />
                             </div>
 
-                            <div className="col-span-full sm:col-span-3">
-                                <label className="text-md font-medium">Location</label>
-                                <input id="location" type="text" name="location"
-                                    defaultValue={datas.location} placeholder="Location" className="w-full rounded-md p-2" />
+                            <div className="col-span-full sm:col-span-3 animate__animated animate__bounceInRight">
+                                <label className="text-md font-medium text-white">Location</label>
+                                <input id="location" type="text"
+                                    defaultValue={datas.location} name="location" placeholder="Location" className="w-full rounded-md p-2" />
                             </div>
-                            <div className="col-span-full sm:col-span-3">
-                                <h1 className="text-md font-medium">Select Category</h1>
+                            <div className="col-span-full sm:col-span-3 animate__animated animate__bounceInLeft">
+                                <h1 className="text-md font-medium text-white">Select Category</h1>
                                 <select className="p-2 rounded-md" name="category" id="">
-                                    <option selected disabled>{datas.category}</option>
+                                    <option disabled selected>{datas.category}</option>
                                     <option value="Health Care">Health Care</option>
                                     <option value="Education">Education</option>
                                     <option value="Social Service">Social Service</option>
                                     <option value="Animal Welfare">Animal Welfare</option>
                                 </select>
                             </div>
-                            <div className="col-span-full sm:col-span-3 flex flex-col">
-                                <label htmlFor="" className="text-md font-medium">Deadline</label>
-                                <DatePicker className="p-2 rounded-md" selected={startDate} onChange={(date) => setStartDate(date)} />
+                            <div className="col-span-full sm:col-span-3 flex flex-col animate__animated animate__bounceInRight">
+                                <label htmlFor="" className="text-md font-medium text-white">Deadline</label>
+                                <DatePicker className="p-2 rounded-md" selected={datas.deadline} onChange={(date) => setStartDate(date)}
+                                    format='yyyy-MM-dd' />
                             </div>
                         </div>
                     </fieldset>
-                    <div className="pl-6">
-                        <p className="font-medium text-lg">Your Information</p>
+                    <div className="w-[96%] mx-auto p-3 border-2 hover:bg-[#546e7a] ">
+                        <p className="font-medium text-2xl text-white text-center">Your Information</p>
 
                     </div>
                     <fieldset className="grid grid-cols-4 gap-6 p-6 pt-2 rounded-md shadow-sm dark:bg-gray-50">
 
-                        <div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
-                            <div className="col-span-full sm:col-span-3">
-                                <label htmlFor="username" className="text-md font-medium">Username</label>
+                        <div className="grid grid-cols-6 gap-4 col-span-full ">
+                            <div className="col-span-full sm:col-span-3 animate__animated animate__bounceInLeft">
+                                <label htmlFor="username" className="text-md font-medium text-white">Username</label>
                                 <input id="username" type="text" name="username"
                                     placeholder="Username" value={user?.displayName} className="w-full rounded-md p-2" />
                             </div>
-                            <div className="col-span-full sm:col-span-3">
-                                <label htmlFor="website" className="text-md font-medium">Email</label>
+                            <div className="col-span-6 sm:col-span-3 animate__animated animate__bounceInRight">
+                                <label htmlFor="" className="text-md font-medium text-white">Email</label>
                                 <input id="email" type="email" name="email"
                                     placeholder="Email" value={user?.email} className="w-full rounded-md p-2" />
                             </div>
@@ -149,8 +154,8 @@ const UpdatedPage = () => {
                         </div>
 
                     </fieldset>
-                    <div>
-                        <input className="btn btn-secondary w-full" type="submit" value="Updated Post" />
+                    <div className="w-[96%] mx-auto">
+                        <input className="btn bg-[#039be5] text-white w-full " type="submit" value="Updated Post" />
                     </div>
                 </form>
             </section>
